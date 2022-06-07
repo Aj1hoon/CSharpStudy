fa9eac4535df107070192c99a9281df9416ed576
Aj1hoon <92YOJH@gmail.com>
2022-06-07 17:31:31 +0900

    2022/06/07 수업예제

---
+++ BackJoon/02단계/Main2_01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJoon._02단계
+{
+    internal class Main2_01
+    {
+        static void Main1(string[] args)
+        {
+
+        }
+    }
+}
---
+++ BackJoon/02단계/Main2_05.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJoon._02단계
+{
+    internal class Main2_05
+    {
+    }
+}
