13888b2c0d7f8a6c1ceb507431ba58ac887e5a3f
Aj1hoon <92YOJH@gmail.com>
2022-06-10 17:27:21 +0900

    2022/06/10 Update

---
+++ Test/2/2_02.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2
+{
+    internal class _2_02
+    {
+        static void Main02(string[] args)
+        {
+            int[] arr = { 17, 92, 18, 33, 58, 7, 26, 42 };
+
+            int minNum = arr[0];
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (minNum > arr[i])
+                {
+                    minNum = arr[i];
+                }
+            }
+            Console.WriteLine("배열 arr에서 가장 작은 수 : " + minNum);
+        }
+    }
+}
