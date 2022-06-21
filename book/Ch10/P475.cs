a40ebf00b0259b15d676f8a74dedc24a905eaf3b
Aj1hoon <92YOJH@gmail.com>
2022-06-21 17:18:51 +0900

    2022/06/21

---
+++ book/Ch10/P475.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book.Ch10
+{
+    internal class P475
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("입력 : ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int index = int.Parse(input);
+                Console.WriteLine("입력 숫자 : " + index);
+            }
+
+            catch (Exception exception)
+            {
+                Console.WriteLine("예외가 발생했습니다.");
+                Console.WriteLine(exception.GetType());
+                Console.WriteLine(exception.Message);
+                Console.WriteLine(exception.StackTrace);
+
+            }
+        }
+    }
+}
