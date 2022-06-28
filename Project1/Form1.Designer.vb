60cc772e1277d47fcec1a1eb832a144895219d85
Aj1hoon <92YOJH@gmail.com>
2022-06-28 17:17:45 +0900

    2022/06/28

---
+++ Project1/Form1.Designer.vb
@@ -0,0 +1,265 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnDiv = New System.Windows.Forms.Button()
+        Me.btnMulti = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.BtnMinus = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btnPlus = New System.Windows.Forms.Button()
+        Me.btnEq = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtResult
+        '
+        Me.txtResult.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.txtResult.Enabled = False
+        Me.txtResult.Font = New System.Drawing.Font("맑은 고딕", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtResult.ForeColor = System.Drawing.SystemColors.MenuText
+        Me.txtResult.Location = New System.Drawing.Point(12, 12)
+        Me.txtResult.Multiline = True
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.ReadOnly = True
+        Me.txtResult.Size = New System.Drawing.Size(287, 70)
+        Me.txtResult.TabIndex = 0
+        Me.txtResult.Text = "0"
+        Me.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btn7
+        '
+        Me.btn7.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn7.Location = New System.Drawing.Point(12, 97)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(67, 67)
+        Me.btn7.TabIndex = 1
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn8.Location = New System.Drawing.Point(85, 97)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(67, 67)
+        Me.btn8.TabIndex = 2
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn9.Location = New System.Drawing.Point(160, 97)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(67, 67)
+        Me.btn9.TabIndex = 3
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btnDiv
+        '
+        Me.btnDiv.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnDiv.Location = New System.Drawing.Point(233, 97)
+        Me.btnDiv.Name = "btnDiv"
+        Me.btnDiv.Size = New System.Drawing.Size(67, 67)
+        Me.btnDiv.TabIndex = 4
+        Me.btnDiv.Text = "/"
+        Me.btnDiv.UseVisualStyleBackColor = True
+        '
+        'btnMulti
+        '
+        Me.btnMulti.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnMulti.Location = New System.Drawing.Point(233, 170)
+        Me.btnMulti.Name = "btnMulti"
+        Me.btnMulti.Size = New System.Drawing.Size(67, 67)
+        Me.btnMulti.TabIndex = 8
+        Me.btnMulti.Text = "*"
+        Me.btnMulti.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn6.Location = New System.Drawing.Point(160, 170)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(67, 67)
+        Me.btn6.TabIndex = 7
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn5.Location = New System.Drawing.Point(85, 170)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(67, 67)
+        Me.btn5.TabIndex = 6
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn4.Location = New System.Drawing.Point(12, 170)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(67, 67)
+        Me.btn4.TabIndex = 5
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'BtnMinus
+        '
+        Me.BtnMinus.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnMinus.Location = New System.Drawing.Point(233, 243)
+        Me.BtnMinus.Name = "BtnMinus"
+        Me.BtnMinus.Size = New System.Drawing.Size(67, 67)
+        Me.BtnMinus.TabIndex = 12
+        Me.BtnMinus.Text = "-"
+        Me.BtnMinus.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn1.Location = New System.Drawing.Point(160, 243)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(67, 67)
+        Me.btn1.TabIndex = 11
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn2.Location = New System.Drawing.Point(85, 243)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(67, 67)
+        Me.btn2.TabIndex = 10
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn3.Location = New System.Drawing.Point(12, 243)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(67, 67)
+        Me.btn3.TabIndex = 9
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btnPlus
+        '
+        Me.btnPlus.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnPlus.Location = New System.Drawing.Point(233, 316)
+        Me.btnPlus.Name = "btnPlus"
+        Me.btnPlus.Size = New System.Drawing.Size(67, 67)
+        Me.btnPlus.TabIndex = 16
+        Me.btnPlus.Text = "+"
+        Me.btnPlus.UseVisualStyleBackColor = True
+        '
+        'btnEq
+        '
+        Me.btnEq.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnEq.Location = New System.Drawing.Point(160, 316)
+        Me.btnEq.Name = "btnEq"
+        Me.btnEq.Size = New System.Drawing.Size(67, 67)
+        Me.btnEq.TabIndex = 15
+        Me.btnEq.Text = "="
+        Me.btnEq.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnCancel.Location = New System.Drawing.Point(85, 316)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(67, 67)
+        Me.btnCancel.TabIndex = 14
+        Me.btnCancel.Text = "C"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn0.Location = New System.Drawing.Point(12, 316)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(67, 67)
+        Me.btn0.TabIndex = 13
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(311, 402)
+        Me.Controls.Add(Me.btnPlus)
+        Me.Controls.Add(Me.btnEq)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.BtnMinus)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btnMulti)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btnDiv)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.txtResult)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtResult As TextBox
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnDiv As Button
+    Friend WithEvents btnMulti As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents BtnMinus As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents btnEq As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btn0 As Button
+End Class
---
+++ Project1/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public parial Class Form1 : Form
+
+    {
+    Private int var1 = 0;
+    Private int var2 = 0;
+    Private int result = 0;
+
+    }
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtResult.TextChanged
+
+End Sub
+
+Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+
+End Sub
+
+Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+
+End Sub
+
+Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+
+End Sub
+
+Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+
+End Sub
+
+Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+
+End Sub
+
+Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+
+End Sub
+
+Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+
+End Sub
+
+Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+
+End Sub
+
+Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+
+End Sub
+
+Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+
+End Sub
+
+Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+
+End Sub
+
+Private Sub btnEq_Click(sender As Object, e As EventArgs) Handles btnEq.Click
+
+End Sub
+
+Private Sub btnDiv_Click(sender As Object, e As EventArgs) Handles btnDiv.Click
+
+End Sub
+
+Private Sub btnMulti_Click(sender As Object, e As EventArgs) Handles btnMulti.Click
+
+End Sub
+
+Private Sub BtnMinus_Click(sender As Object, e As EventArgs) Handles BtnMinus.Click
+
+End Sub
+
+Private Sub btnPlus_Click(sender As Object, e As EventArgs) Handles btnPlus.Click
+
+End Sub
+End Class
